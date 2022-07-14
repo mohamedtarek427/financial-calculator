abaf7f5af331dc48f8a0294cf0e46f7a1f37f878
Mohamed Tarek <102771793+mohamedtarek427@users.noreply.github.com>
2022-07-14 12:40:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const amex = document.querySelector('.amex');
 
 document.querySelector('.brex').addEventListener('click', 
 
-function bradReward(e) {
+function brexReward(e) {
     const vendorBills = document.querySelector('.vendorBillsValue');
     const vendorBillsValue = parseFloat(vendorBills.value);
     
@@ -18,7 +18,68 @@ function bradReward(e) {
     const teamProjectValue = parseFloat(teamProject.value);
     
     const sum = vendorBillsValue + corpSpendValue + mediaADValue + teamProjectValue;
-    document.querySelector('.brexRewardValue').innerHTML = sum * 0.3
+    document.querySelector('.brandRewardValue').innerHTML = sum * 0.3;
+    document.querySelector('.unlimitedRewards').innerHTML = sum * 1.1;
+    document.querySelector('.lifetimeRewards').innerHTML = sum * 1.2;
     e.preventDefault()
     }
 )
+
+
+document.querySelector('.stripe').addEventListener('click', 
+
+function stripeReward(e) {
+    const vendorBills = document.querySelector('.vendorBillsValue');
+    const vendorBillsValue = parseFloat(vendorBills.value);
+    
+    const corpSpend = document.querySelector('.corpSpend');
+    const corpSpendValue = parseFloat(corpSpend.value);
+    
+    const mediaAD = document.querySelector('.mediaAD');
+    const mediaADValue = parseFloat(mediaAD.value);
+    
+    const teamProject = document.querySelector('.teamProject');
+    const teamProjectValue = parseFloat(teamProject.value);
+    
+    const sum = vendorBillsValue + corpSpendValue + mediaADValue + teamProjectValue;
+    document.querySelector('.brandRewardValue').innerHTML = sum * 2.9
+    document.querySelector('.unlimitedRewards').innerHTML = sum * 1.1;
+    document.querySelector('.lifetimeRewards').innerHTML = sum * 1.2;
+    e.preventDefault()
+    }
+)
+
+document.querySelector('.amex').addEventListener('click', 
+
+function amexReward(e) {
+    const vendorBills = document.querySelector('.vendorBillsValue');
+    const vendorBillsValue = parseFloat(vendorBills.value);
+    
+    const corpSpend = document.querySelector('.corpSpend');
+    const corpSpendValue = parseFloat(corpSpend.value);
+    
+    const mediaAD = document.querySelector('.mediaAD');
+    const mediaADValue = parseFloat(mediaAD.value);
+    
+    const teamProject = document.querySelector('.teamProject');
+    const teamProjectValue = parseFloat(teamProject.value);
+    
+    const sum = vendorBillsValue + corpSpendValue + mediaADValue + teamProjectValue;
+    document.querySelector('.brandRewardValue').innerHTML =  sum * 1.3
+    document.querySelector('.unlimitedRewards').innerHTML = sum * 1.1;
+    document.querySelector('.lifetimeRewards').innerHTML = sum * 1.2;
+    e.preventDefault()
+    }
+)
+function textHandlerBrex () {
+   document.querySelector('.brand-name').innerHTML = "Brex Business Card"
+}
+
+function textHandlerStripe () {
+    document.querySelector('.brand-name').innerHTML = "stripe Business Card"
+ }
+
+ function textHandlerAmex () {
+    document.querySelector('.brand-name').innerHTML = "amex Business Card"
+ }
+
